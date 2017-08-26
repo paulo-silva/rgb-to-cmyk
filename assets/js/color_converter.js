13fc543dc8ea73a1aaacc79331e526ad61c75db1
pauloh-silva <paulo_silvalearn@hotmail.com>
2017-08-26 17:57:16 -0300

    First commit

---
+++ assets/js/color_converter.js
@@ -0,0 +1,55 @@
+/**
+ * Classe responsável por converter RGB para CMYK ou CMYK para RGB
+ * @author  Paulo H. da Silva
+ */
+class ColorConverter {
+
+  /**
+   * Converte RGB para CMYK
+   * @param  {integer} red   Código referente a quantidade de vermelho
+   * @param  {integer} green Código referente a quantidade de verde
+   * @param  {integer} blue  Código referente a quantidade de azul
+   * @return {object}        Valores de ciano, magenta, amarelo e preto
+   * @author Paulo H. da Silva
+   */
+  rgbToCmyk(red, green, blue) {
+
+    red = (parseInt(red) | 0) / 255;
+    green = (parseInt(green) | 0) / 255;
+    blue = (parseInt(blue) | 0) / 255;
+
+    let black = 1 - Math.max(red, green, blue)
+      , cyan = ( (1 - red - black) / (1 - black) )
+      , magenta = ( (1 - green - black) / (1 - black) )
+      , yellow = ( (1 - blue - black) / (1 - black) )
+    ;
+
+    return {
+      cyan: parseFloat(cyan).toFixed(3),
+      magenta: parseFloat(magenta).toFixed(3),
+      yellow: parseFloat(yellow).toFixed(3),
+      black: parseFloat(black).toFixed(3)
+    };
+  }
+
+  /**
+   * Converte CMYK para RGB
+   * @param  {integer} cyan     Código referente a quantidade de ciano
+   * @param  {integer} magenta  Código referente a quantidade de magenta
+   * @param  {integer} yellow   Código referente a quantidade de amarelo
+   * @param  {integer} black    Código referente a quantidade de preto
+   * @return {object}           Valores de vermelho, verde e azul
+   */
+  cmykToRgb(cyan, magenta, yellow, black) {
+    let red = 255 * (1 - parseInt(cyan) | 0)* (1 - black)
+      , green = 255 * (1 - parseInt(magenta) | 0) * (1 - black)
+      , blue = 255 * (1 - parseInt(yellow) | 0) * (1 - black)
+    ;
+
+    return {
+      red: 255 - cyan,
+      green: 255 - magenta,
+      blue: 255 - yellow
+    };
+  }
+}
---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+  let colorsInput = document.getElementsByClassName('color-input')
+    , totalColorsInputs = colorsInput.length
+    , colorConverter = new ColorConverter()
+  ;
+
+  for (let i = 0; i < totalColorsInputs; i++) {
+
+    colorsInput[i].addEventListener('input', function(event) {
+      let colorFormat = event.currentTarget.parentNode.dataset.format;
+
+      calculateColorsValues(colorFormat);
+
+    }); 
+  }
+
+  function calculateColorsValues(colorFormat) {
+
+    let values = 'rgb';
+    if (colorFormat === 'rgb') {
+      let red = parseInt(document.querySelector('[data-color=red]').value | 0)
+        , green = parseInt(document.querySelector('[data-color=green]').value | 0)
+        , blue = parseInt(document.querySelector('[data-color=blue]').value | 0)
+        , sumColor = red + green + blue
+      ;
+
+      values = colorConverter.rgbToCmyk(red, green, blue);
+      document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+
+      if (sumColor > 600) {
+        document.body.classList.add('dark');
+        document.body.classList.remove('clean');
+      } else {
+        document.body.classList.add('clean');
+        document.body.classList.remove('dark');
+      }
+
+
+      colorFormat = 'cmyk';
+    } else if (colorFormat === 'cmyk') {
+      let cyan = document.querySelector('[data-color=cyan]').value | 0
+        , magenta = document.querySelector('[data-color=magenta]').value | 0
+        , yellow = document.querySelector('[data-color=yellow]').value | 0
+        , black = document.querySelector('[data-color=black]').value | 0
+      ;
+      values = colorConverter.cmykToRgb(cyan, magenta, yellow, black);
+      colorFormat = 'rgb';
+    } else {
+      alert(`Formato ${colorFormat.toUpperCase()} inválido`);
+      return;
+    }
+    fillColorsInputs(colorFormat, values);
+
+  }
+
+  function fillColorsInputs(colorFormat, values) {
+    
+    for (let i in values) {
+      document.getElementById(`${colorFormat}_${i}`).value = values[i];
+    }
+  }
+
+});
